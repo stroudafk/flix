77df31a8d7ff2323f18c5e1e455b171eeec253d3
SJ Stroud <stroudafk@gmail.com>
2021-06-23 18:36:00 -0500

    -pre activity indicator...

---
+++ flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.h
+//  flix
+//
+//  Created by Sj Stroud on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
