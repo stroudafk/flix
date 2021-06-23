59bec7b240ca0a78d521c51dfa5a126c62c98dc0
SJ Stroud <stroudafk@gmail.com>
2021-06-23 10:48:43 -0500

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by Sj Stroud on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ flix/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  flix
+//
+//  Created by Sj Stroud on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
