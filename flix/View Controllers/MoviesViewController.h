94891033f92c8c460f644b19c9f8e7672acf73f9
SJ Stroud <stroudafk@gmail.com>
2021-06-23 14:18:03 -0500

    -linked code to the movies showing in theaters now -prints all titles to console log

---
+++ flix/View Controllers/MoviesViewController.h
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.h
+//  flix
+//
+//  Created by Sj Stroud on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
